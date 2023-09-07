a7cf9729ce77417bce2a9cdf94958602d4a1b854
NelsonIvan003 <nelsonivan003@gmail.com>
2023-09-07 11:41:53 -0300

    Se crea lista de diccionario -> id del 1 al 10, age random del 1 al 100

--- simple/simple.py
+++ simple/simple.py
@@ -2,8 +2,20 @@ import random
 
 
 def simple_list():
+    list_dict = []
+    for id in range(1, 11):
+        data_dict = {"id": id, "age": random.randint(1, 100)}
+        list_dict.append(data_dict)
+
+    print(list_dict)
+    return list_dict
+
+
+def sort_list(l_dict=None):
+    print("Retornar la lista ordenada por edades --> # lo que podría agregar es que si hay dos edades iguales entonces"
+          "se mostrará primero el menor id")
     pass
 
 
-def sort_list():
-    pass
+simple_list()
+sort_list()
