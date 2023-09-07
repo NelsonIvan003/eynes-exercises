72b3c44f9f6daba7fa555b70120e398a4cfc86fd
NelsonIvan003 <nelsonivan003@gmail.com>
2023-09-07 12:44:08 -0300

    Instanciar círculo

--- circle/circle.py
+++ circle/circle.py
@@ -1,4 +1,34 @@
 import math
 
-class Circle():
-    pass
+
+class Circle:
+    def __init__(self, r):
+        if r <= 0:
+            raise ValueError("Para crear el círculo, el radio debe ser mayor que cero. "
+                             f"Intentó ingresar como radio: {r}")
+
+        self.radius = r
+        print(f"Haz creado exitosamente un círculo de radio: {self.radius}")
+
+
+# circle_1 error
+try:
+    circle_1 = Circle(0)
+except ValueError as e1:
+    print("Error:", e1)
+
+# circle_2 error
+try:
+    circle_2 = Circle(-2)
+except ValueError as e2:
+    print("Error:", e2)
+
+# circle_3 success
+try:
+    circle_3 = Circle(5)
+except ValueError as e3:
+    print("Error:", e3)
+
+
+
+
