88196e74b2c6d2a3cf88867a050ccf7808ccbfcf
NelsonIvan003 <nelsonivan003@gmail.com>
2023-09-07 14:20:00 -0300

    pruebas realizadas ok

--- matrix/transpose.py
+++ matrix/transpose.py
@@ -7,9 +7,7 @@ matrix[i][j] --> transp[j][i]
 
 def transpose(matrix):
     f = len(matrix)
-    print(f)
     c = len(matrix[0])
-    print(c)
 
     transposed_matrix = []
     for _ in range(c):
@@ -20,17 +18,4 @@ def transpose(matrix):
             transposed_matrix[j][i] = matrix[i][j]
 
     return transposed_matrix
-
-# Prueba con los ej del readme
-input1 = [[1, 2]]
-print(input1)
-print(transpose(input1))
-
-input2 = [[1, 2], [3, 4], [5, 6]]
-print(input2)
-print(transpose(input2))
-
-input3 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-print(input3)
-print(transpose(input3))
 
